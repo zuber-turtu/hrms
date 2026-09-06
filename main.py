0d4073409b3e59e609e0e558342be26c4c5f68e8
zuber-turtu <zuber@turtu.in>
2026-09-06 19:26:25 +0530

    fix: update uvicorn app reference in main.py and remove redundant migration script

--- main.py
+++ main.py
@@ -245,4 +245,4 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main.py:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
